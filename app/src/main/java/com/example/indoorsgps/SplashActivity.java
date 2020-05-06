c0219d62000f77fb529ac4ce332956e45e241488
priyankamadhwal <priyanka0701m@gmail.com>
2020-05-07 01:31:48 +0530

    Adding a splash screen

---
+++ app/src/main/java/com/example/indoorsgps/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.indoorsgps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (isSignedIn()) {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        }
+        else {
+            startActivity(new Intent(SplashActivity.this, SignInActivity.class));
+        }
+    }
+
+    private boolean isSignedIn() {
+        return (GoogleSignIn.getLastSignedInAccount(this) != null);
+    }
+}
