8b80a4bf9462f861262598ceba8163d0f6d5fa7b
priyankamadhwal <priyanka0701m@gmail.com>
2020-05-07 18:01:07 +0530

    Profile fragment ui

---
+++ app/src/main/java/com/example/indoorsgps/ProfileFragment.java
@@ -0,0 +1,53 @@
+package com.example.indoorsgps;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.squareup.picasso.Picasso;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // Update profile
+
+        TextView userIdView = root.findViewById(R.id.userIdView);
+        userIdView.setText("ID: " + GoogleSignIn.getLastSignedInAccount(getActivity().getApplicationContext()).getId());
+
+        ImageView userProfileImageView = root.findViewById(R.id.userProfileImageView);
+        Uri userProfileImageUrl = GoogleSignIn.getLastSignedInAccount(getActivity().getApplicationContext()).getPhotoUrl();
+        Picasso.get().load(userProfileImageUrl.toString())
+                .placeholder(R.drawable.profile_picture_placeholder)
+                .error(R.drawable.profile_picture_placeholder)
+                .into(userProfileImageView);
+
+        TextView userDisplayNameView = root.findViewById(R.id.userDisplayNameView);
+        userDisplayNameView.setText(GoogleSignIn.getLastSignedInAccount(getActivity().getApplicationContext()).getDisplayName());
+
+        TextView userEmailView = root.findViewById(R.id.userEmailView);
+        userEmailView.setText(GoogleSignIn.getLastSignedInAccount(getActivity().getApplicationContext()).getEmail());
+
+        return root;
+    }
+}
