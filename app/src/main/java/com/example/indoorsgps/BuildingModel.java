3ab74757bf7e021117461c25c4d81e614c9f0119
Priyanka Madhwal <priyanka0701m@gmail.com>
2020-05-08 11:16:46 +0530

    Update BuildingModel.java

--- app/src/main/java/com/example/indoorsgps/BuildingModel.java
+++ app/src/main/java/com/example/indoorsgps/BuildingModel.java
@@ -3,12 +3,17 @@ package com.example.indoorsgps;
 public class BuildingModel {
 
     private String id;
+    private String address;
     private double latitude;
     private double longitude;
     private float radius;
 
     public String getId() {
         return id;
+    }
+    
+     public String getAddress() {
+        return address;
     }
 
     public double getLatitude() {
